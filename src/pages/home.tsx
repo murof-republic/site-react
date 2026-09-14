ddbf570d8b7c98e614505f83b9f722171fa371ab
Trigger <triggerww@outlook.com>
2026-09-14 09:29:48 -0300

    ajuste de componentes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,26 @@
 import './css/home.css'
-import Intro from '../components/intro/intro'
 import CardIntro from '../components/cards/card-intro'
+import discord from "../assets/images/discord.webp"
 import Trigger from '../assets/images/icon_user/6321f16d3c7dbeba8e0975a3929b1b20.webp'
-import snuf from '../assets/images/icon_user/24d834316d889fb31dd8a393f3df3e2e.webp'
+import Snuf from '../assets/images/icon_user/24d834316d889fb31dd8a393f3df3e2e.webp'
 
 function Home() {
     return(
         <main>
             <article>
                 <div id="background"></div>
-                <Intro/>
+                <section>
+                    <div id='intro-content'>
+                        <h2>Olá viajante!</h2>
+                        <p>Unimos entusiastas e profissionais de programação, tecnologia, jogos e arte em um ambiente feito para a troca de conhecimento e boas conexões. Faça parte!</p>
+                    </div>
+
+                    <div id='intro-social'>
+                        <a href="https://discord.gg/MWrYrytMCg" target="_blank" rel="noopener noreferrer">
+                            <img src={discord} width={35} height={35} alt="Nosso Discord!" />
+                        </a>
+                    </div>
+                </section>
                 <section className="cards">
                     <CardIntro
                         imagem={Trigger}
@@ -21,7 +32,7 @@ function Home() {
                     />
 
                     <CardIntro
-                        imagem={snuf}
+                        imagem={Snuf}
                         nome="Snuf"
                         conteudo={<>
                                 Adicione o Snuf ao seu servidor!<br/>
